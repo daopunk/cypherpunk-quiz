0134681468e2baa7b5fbba2f9a1c7a744e4f1c7a
hking2 <50965054+hking2@users.noreply.github.com>
2019-11-08 17:59:06 -0600

    Update script.js

--- script.js
+++ script.js
@@ -122,33 +122,27 @@ function renderQuestion () {
 function generateQuestion() {
   if (questionNum < store.length) {
 
-    return `<div class="question-${questionNum}">
+    return `
+    <form>
+    <div class="question-${questionNum}">
     <h2 class='quizQuestion'>${store[questionNum].question}</h2>
-    <form><fieldset class = 'answerOption'>
+    <fieldset class = 'answerOption'>
 
-    <section role="answerOption">
     <label for="ans1">
     <input id="ans1" type='radio' value="${store[questionNum].answers[0]}" name="option" required><span> ${store[questionNum].answers[0]}</span>
     </label>
-    </section>
 
-    <section role="answerOption">
     <label for="ans2">
     <input id="ans2" type='radio' value="${store[questionNum].answers[1]}" name="option" required><span> ${store[questionNum].answers[1]}</span>
     </label>
-    </section>
 
-    <section role="answerOption">
     <label for="ans3">
     <input id="ans3" type='radio' value="${store[questionNum].answers[2]}" name="option" required><span> ${store[questionNum].answers[2]}</span>
     </label>
-    </section>
 
-    <section role="answerOption">
     <label for="ans4">
     <input id="ans4" type='radio' value="${store[questionNum].answers[3]}" name="option" required><span> ${store[questionNum].answers[3]}</span>
     </label>
-    </section>
 
     <button type='submit' class="submitButton">Submit</button>
 
